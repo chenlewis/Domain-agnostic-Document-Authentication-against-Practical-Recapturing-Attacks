742f1878e37f8f2315a6572f3593e7c1be72b3c0
Changsheng CHEN <chenlewis@126.com>
2022-08-22 10:43:54 +0800

    Add files via upload

--- code/utils/utils.py
+++ code/utils/utils.py
@@ -1,1 +1,13 @@
+import torch
+import csv
 
+def val_score_siamese(score_ap, score_an):
+    score = torch.exp(score_ap) / (torch.exp(score_ap) + torch.exp(score_an))
+    return score
+
+def write_csv(results, file_name):
+
+    with open(file_name, 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(['id', 'label'])
+        writer.writerows(results)
